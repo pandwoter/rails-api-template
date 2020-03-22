a004a0691a24342e0fab9203d2cbb31b8303d245
Admin <alex@ubuntu.ubuntu-domain>
2020-03-23 00:12:16 +0300

    added additional logic

--- generate_template.rb
+++ generate_template.rb
@@ -23,7 +23,7 @@ module ApiTemplate
 
       # Generate Template command with many amazing options!
       class GenerateApiTemplate < Dry::CLI::Command
-        desc 'Generates ready for use API template'.colorize :blue
+        desc 'Generates ready for use API'.colorize :blue
 
         argument :app_name,          required: true,  desc: 'Application name'
         argument :database,          required: true,  desc: 'Application DB (sqlite if not specified)'
@@ -33,24 +33,36 @@ module ApiTemplate
         argument :rspec,             required: false, desc: 'Configure RSpec?'
         argument :jwt_auth_template, required: false, desc: 'Scaffolds jwt-auth'
 
-        def call(**options)
-          puts '[API generation] started!'.colorize :green
-          extend FileUtils
+        def copy_file_wrapper(src, dest, color)
+          puts "Copying #{src} to #{dest}...".colorize(color)
+          cp(src, dest)
+          puts 'Copied...'.colorize(color)
+        end
 
+        def call(**options)
+          extend FileUtils
+          puts '[API generation] started!'.colorize :green
           puts '[API generation] checking rails installation'.colorize :green
           raise RailsNotInstalledError unless `gem list` =~ /rails/
 
           puts '[API generation] creating rails folder'.colorize :green
-          raise UnknownDatabase unless %w{mysql postgresql sqlite}.include? options[:database]
-          system("rails new #{options[:app_name]} --api --database=#{options[:database]}")
+          raise UnknownDatabase unless %w[mysql postgresql sqlite].include? options[:database]
 
+          system("rails new #{options[:app_name]} --api --database=#{options[:database]}")
           puts '[API generation] rails folder has been created'.colorize :green
 
-          puts '[API generation] Copying Gemfile'.colorize :green
-          puts '[API generation] Copying Solargraph config'.colorize :green
-          puts '[API generation] Copying Rubocop config'.colorize :green
-          puts '[API generation] Copying Pryrc config'.colorize :green
-          puts '[API generation] Copying RSpec config'.colorize :green
+          copy_file_wrapper('./lib/Gemfile',         "./#{options[:app_name]}", :green)
+          rm "./#{options[:app_name]}/Gemfile.lock", force: true
+          copy_file_wrapper('./lib/.solargraph.yml', "./#{options[:app_name]}", :green)
+          copy_file_wrapper('./lib/.rubocop.yml',    "./#{options[:app_name]}", :green)
+          copy_file_wrapper('./lib/.pryrc',          "./#{options[:app_name]}", :green)
+
+          puts '[API generation] Set-up RSpec configuration'.colorize :green
+          puts '[API generation] Removing default tests folder'.colorize :green
+          rm_r "./#{options[:app_name]}/test", force: true
+          cp_r('./lib/spec', "./#{options[:app_name]}")
+          copy_file_wrapper('./lib/.rspec', "./#{options[:app_name]}", :green)
+
           puts '[API generation] Bootstraping simple jwt auth'.colorize :green
         end
       end
